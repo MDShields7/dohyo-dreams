631a22de84e31a9b373f511610e2f2dacbaf6c52
Matt Shields <matthew.duncan.shields@gmail.com>
2019-05-16 03:18:06 -0500

    nav sticky, improved responsive menu

--- src/Admin/Admin.js
+++ src/Admin/Admin.js
@@ -4,7 +4,8 @@ export default class Admin extends Component {
   render() {
     return (
       <div className='container'>
-        ADMIN LOGIN
+        <h1>Admin Login</h1>
+
       </div>
     )
   }
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
 import { Link, withRouter } from "react-router-dom";
-import logo from "../assets/dohyo-logo-36h.png"
+import logo from "../assets/dohyo-logo-36h.png";
+import Menu from "../assets/menu-dark.svg"
+import MenuOn from "../assets/menu-light.svg"
 import './Navbar.scss';
 
 export default class Navbar extends Component {
   constructor() {
     super();
     this.state = {
-      toggleNav: false
+      toggleNav: false,
     }
   }
   toggleNav = () => {
@@ -18,18 +20,22 @@ export default class Navbar extends Component {
   }
   render() {
     return (
-      <nav className='nav'>
-        <button onClick={this.toggleNav}></button>
+      <nav>
+        {this.state.toggleNav ?
+          <img className='menu' src={MenuOn} alt='menu icon' onClick={this.toggleNav} /> :
+          <img className='menu' src={Menu} alt='menu icon' onClick={this.toggleNav} />
+        }
         <div className='container'>
           <div className='row space-between'>
 
             <Link className="nav-logo" to="/">
               <img src={logo} alt="" />
             </Link>
-            <section className={this.state.toggleNav ? 'show' : ''}>
+            <span className={this.state.toggleNav ? 'show-2' : ''} onClick={this.toggleNav} />
+            <section className={this.state.toggleNav ? 'show' : ''} onClick={this.toggleNav}>
               <ul>
                 <li>
-                  <Link className="nav-text" to="/about"> Admin</Link>
+                  <Link className="nav-text" to="/admin"> Admin</Link>
                 </li>
                 <li>
                   <Link className="nav-text" to="/about">About</Link>
